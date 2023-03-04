65c57efbb501436fc31baecedf6db65000de46e9
Шичев Никита Алексеевич <84218925+KoliVans@users.noreply.github.com>
2023-03-04 21:40:39 +0300

    pop up

--- js/product.js
+++ js/product.js
@@ -5,21 +5,28 @@
 	  return;
 	}
  
+	var changeProductSize = function(target) {
+	  var product = myLib.closestItemByClass(target, 'product');
+ 
+	  previousBtnActive.classList.remove('is-active');
+	  target.classList.add('is-active');
+	};
+ 
 	var changeProductOrderInfo = function(target) {
-		var product = myLib.closestItemByClass(target, 'product');
-		var order = document.querySelector ('.popup-order');
-  
-		var productTitle = product.querySelector('.product__title').textContent;
-		var productPrice = product.querySelector ('.product__price-value').textContent;
-		var productImgSrc = product.querySelector('.product__img').getAttribute('src');
-  
-		order.querySelector('.order-info-title').setAttribute('value', productTitle);
-		order.querySelector('.order-info-price').setAttribute('value', productPrice);
-  
-		order.querySelector('.order-product-title').textContent = productTitle;
-		order.querySelector('.order-product-price').textContent = productPrice;
-		order.querySelector('.order__img').setAttribute('src', productImgSrc);
-	 };
+	  var product = myLib.closestItemByClass(target, 'product');
+	  var order = document.querySelector('.popup-order');
+ 
+	  var productTitle = product.querySelector('.product__title').textContent;
+	  var productPrice = product.querySelector('.product__price-value').textContent;
+	  var productImgSrc = product.querySelector('.product__img').getAttribute('src');
+ 
+	  order.querySelector('.order-info-title').setAttribute('value', productTitle);
+	  order.querySelector('.order-info-price').setAttribute('value', productPrice);
+ 
+	  order.querySelector('.order-product-title').textContent = productTitle;
+	  order.querySelector('.order-product-price').textContent = productPrice;
+	  order.querySelector('.order__img').setAttribute('src', productImgSrc);
+	};
  
 	catalog.addEventListener('click', function(e) {
 	  var target = e.target;
